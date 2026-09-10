ce0ee040789633c8fb8f68f9c9e12b7bbe9caef9
FelipeLafin <felahau@gmail.com>
2026-09-10 18:56:32 -0300

    mais um ajuste

--- integration/src/main/java/br/com/tasty/restauranteservice/feign/RestauranteFeign.java
+++ integration/src/main/java/br/com/tasty/restauranteservice/feign/RestauranteFeign.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 )
 public interface RestauranteFeign {
 
-    @GetMapping("/v1/restaurante/dashboard")
-    RestauranteResponseIntegration dashboard(@RequestParam("codigoUsuario") Integer codigoUsuario);
+    @GetMapping("/tasty-api-service/v1/restaurante/dashboard")
+    RestauranteResponseIntegration dashboard(@RequestParam("sub") Integer codigoUsuario);
 
 }
