8a566ff72fb65414e5f62c280232d7bbc993ddf0
FelipeLafin <felahau@gmail.com>
2026-09-11 09:47:45 -0300

    ajustes arquivos

--- impl/src/main/java/br/com/tasty/autenticacaoservice/feign/AutenticacaoFeign.java
+++ impl/src/main/java/br/com/tasty/autenticacaoservice/feign/AutenticacaoFeign.java
@@ -4,12 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(
-        name = "${openFeign.autenticacao-service.name}",
-        url = "${openFeign.autenticacao-service.url}"
-)
+@FeignClient(name = "${openFeign.tasty-auth-service.name}", url = "${openFeign.tasty-auth-service.url}")
 public interface AutenticacaoFeign {
-
     @GetMapping(value = "/tasty-auth-service/v1/oauth/token/info")
     String validarToken(@RequestHeader("accessToken") String accessToken);
 }
--- integration/src/main/java/br/com/tasty/restauranteservice/feign/RestauranteFeign.java
+++ integration/src/main/java/br/com/tasty/restauranteservice/feign/RestauranteFeign.java
@@ -1,18 +1,13 @@
 package br.com.tasty.restauranteservice.feign;
 
-
 import br.com.tasty.restauranteservice.model.response.RestauranteResponseIntegration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(
-        name = "${openFeign.restaurante-service.name}",
-        url = "${openFeign.restaurante-service.url}"
-)
+@FeignClient(name = "${openFeign.tasty-api-service.name}", url = "${openFeign.tasty-api-service.url}")
 public interface RestauranteFeign {
-
     @GetMapping("/tasty-api-service/v1/restaurante/dashboard")
-    RestauranteResponseIntegration dashboard(@RequestParam("sub") Integer codigoUsuario);
+    RestauranteResponseIntegration dashboard(@RequestParam("codigoUsuario") Integer codigoUsuario);
 
 }
